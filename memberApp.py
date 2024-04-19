90104936c410e7a67ce3ffd371b9e898c5faec3b
golden-dust <jiny.kim221@gmail.com>
2024-04-19 12:35:51 +0900

    회원관리 앱 (04/19/24) - 회원가입 기능 추가중 2 - 회원가입 시 유효성 체크 추가

--- memberApp.py
+++ memberApp.py
@@ -22,22 +22,29 @@ class MainWindow(form_class, QMainWindow):
         input_pw = self.joinpw_edit.text()
         input_name = self.joinname_edit.text()
         input_email = self.joinemail_edit.text()
-        input_age = int(self.joinage_edit.text())
+        input_age = self.joinage_edit.text()
 
-        dbConn = pymysql.connect(user="root", password="12345", host="localhost", db="shopdb")
-
-        sql = f"INSERT INTO appmember VALUES ('{input_id}', '{input_pw}', '{input_name}', '{input_email}', '{input_age}')"
-
-        cur = dbConn.cursor()
-        result = cur.execute(sql)
-        if result == 1:
-            QMessageBox.warning(self, "회원가입성공", "축하합니다!\n회원가입이 완료되었습니다.")
-            self.join_reset()   # 회원가입 성공 ok 클릭 후 입력내용 초기화
+        if input_id == "" or input_pw == "" or input_name == "" or input_email == "" or input_age == "":
+            QMessageBox.warning(self, "정보입력오류", "회원 정보 중 입력되지 않은 정보가 있습니다.\n모든 정보를 입력해주세요.")
+        elif len(input_id) < 4 or len(input_id) > 14:
+            QMessageBox.warning(self, "아이디입력오류", "아이디는 4글자 이상, 14자 이하이어야 합니다.\n다시 입력해주세요.")
+        elif len(input_pw) < 4 or len(input_pw) > 14:
+            QMessageBox.warning(self, "비밀번호입력오류", "비밀번호는 4글자 이상, 14자 이하이어야 합니다.\n다시 입력해주세요.")
         else:
-            QMessageBox.warning(self, "회원가입실패", "회원가입에 실패하였습니다.")
-        cur.close()
-        dbConn.commit()
-        dbConn.close()
+            dbConn = pymysql.connect(user="root", password="12345", host="localhost", db="shopdb")
+
+            sql = f"INSERT INTO appmember VALUES ('{input_id}', '{input_pw}', '{input_name}', '{input_email}', '{input_age}')"
+
+            cur = dbConn.cursor()
+            result = cur.execute(sql)
+            if result == 1:
+                QMessageBox.warning(self, "회원가입성공", "축하합니다!\n회원가입이 완료되었습니다.")
+                self.join_reset()   # 회원가입 성공 ok 클릭 후 입력내용 초기화
+            else:
+                QMessageBox.warning(self, "회원가입실패", "회원가입에 실패하였습니다.")
+            cur.close()
+            dbConn.commit()
+            dbConn.close()
 
     def join_reset(self):   # 회원가입정보 입력내용 초기화
         self.joinid_edit.clear()
